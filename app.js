9ab1729a38dc6386569d13097f4d59b4aa0e628a
Mark McNaughton <markusmack@hotmail.com>
2024-10-28 11:35:56 +0000

    Updated event

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({extended: true}));
 
 //routes
 app.get('/event-details', (req, res) => {
-    F1Events.findOne({id: 20}, 'id event_name circuit event_start')
+    F1Events.findOne({id: 21}, 'id event_name circuit event_start')
     .then((result) => {
         //console.log(result);
         res.send(result);
